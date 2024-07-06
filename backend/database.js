dac1120249f5ab686fc4d0f1f3119bcb4cacdd55
pawara101 <pawara101dassanayake@gmail.com>
2024-07-07 01:21:18 +0530

    database.js corrected | editStudent

--- backend/database.js
+++ backend/database.js
@@ -135,20 +135,19 @@ const addStudent = async (id, name, age, hometown) => {
     });
 }
 
-const updateStudent = async (name, age, hometown, id) => {
-    const sql = `UPDATE student SET name=? age=? hometown=? WHERE id=?`
+const updateStudent = async (id, name, age, hometown) => {
+    const sql = `UPDATE student SET name=?, age=?, hometown=? WHERE id=?`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql,[name, age, hometown, id])
+            .raw(sql, [id, name, age, hometown])
             .then(() => {
-                resolve({status: "Successfully updated student"});
+                resolve({status: "Successfully updated Teacher"})
             })
             .catch((error) => {
                 reject(error);
             });
     });
-} 
-
+}
 const deleteStudent = async (id) => {
     const sql = `DELETE FROM student WHERE id=?`
     return new Promise((resolve, reject) => {
